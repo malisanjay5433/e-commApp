0aaf33823c84ac99c978b639ca9add1880a09979
Sanjay Mali <malisanjay5433@gmail.com>
2020-06-25 12:20:38 +0530

    Initial Commit

---
+++ E-CommerceApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  E-CommerceApp
+//
+//  Created by Sanjay Mali on 25/06/20.
+//  Copyright © 2020 theappmaker. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ E-CommerceAppTests/E_CommerceAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  E_CommerceAppTests.swift
+//  E-CommerceAppTests
+//
+//  Created by Sanjay Mali on 25/06/20.
+//  Copyright © 2020 theappmaker. All rights reserved.
+//
+
+import XCTest
+@testable import E_CommerceApp
+
+class E_CommerceAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
